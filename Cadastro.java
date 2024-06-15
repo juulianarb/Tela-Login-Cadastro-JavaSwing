450620de37ee4e67f4a1c9dbfb5acb4d1fba5dc0
Juliana Rodrigues <127902970+juulianarb@users.noreply.github.com>
2024-06-15 00:25:25 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,9 @@
+
+public class Cadastro {
+
+    public void setVisible(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
+    }
+
+}
